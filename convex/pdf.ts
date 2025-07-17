9d2204076e35a6adef2815ff5f1fb6ebc6bc7f2d
ihtesham510 <ihteshamulhaq510@gamil.com>
2025-07-18 01:58:25 +0500

    fixed use node issue

--- convex/pdf.ts
+++ convex/pdf.ts
@@ -1,21 +1,11 @@
 'use node'
 
-import { internalAction, internalMutation } from './_generated/server'
+import { internalAction } from './_generated/server'
 import { v } from 'convex/values'
 
 export const generatePDF = internalAction({
   args: { data: v.any() },
   async handler(_, args) {
     console.log(args.data)
-  },
-})
-
-export const store_file = internalMutation({
-  args: {
-    storageId: v.id('_storage'),
-    url: v.string(),
-  },
-  async handler(ctx, args) {
-    return await ctx.db.insert('files', args)
   },
 })
