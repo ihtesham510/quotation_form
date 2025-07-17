27c10a636b9844faacc2a13b59956b210ae81b0b
ihtesham510 <ihteshamulhaq510@gamil.com>
2025-07-18 01:56:24 +0500

    fixed pdf issue

--- convex/pdf.ts
+++ convex/pdf.ts
@@ -1,22 +1,12 @@
 'use node'
 
-import { internal } from './_generated/api'
 import { internalAction, internalMutation } from './_generated/server'
 import { v } from 'convex/values'
-import { generatePDFQuote } from '../src/lib/pdf'
-import type { QuoteData } from './pdf_generate_schema'
 
 export const generatePDF = internalAction({
   args: { data: v.any() },
-  async handler(ctx, args) {
-    const data: QuoteData = args.data
-    const blob = await generatePDFQuote(data)
-    const id = await ctx.storage.store(blob)
-    const url = await ctx.storage.getUrl(id)
-    if (url && id) {
-      await ctx.runMutation(internal.pdf.store_file, { storageId: id, url })
-      return url
-    }
+  async handler(_, args) {
+    console.log(args.data)
   },
 })
 
